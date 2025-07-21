05d9ec5b335fb4b118788a2c29f495da5a018feb
ozaki-miso <takuya080808@gmail.com>
2025-07-21 22:56:47 +0900

    Update result_logic.js

--- result_logic.js
+++ result_logic.js
@@ -6,13 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("name").textContent = data.name;
   document.getElementById("yomi").textContent = "（" + data.yomi + "）";
   document.getElementById("origin").textContent = data.origin || "（名前の由来がここに入ります）";
-
-  
-  const fortune = Object.fromEntries(
-  ["love", "study", "money", "home", "health"].map(key => [key, weightedRandomRank()])
-);
- 
-  
   const categories = ["love", "study", "money", "home", "health"];
   const fortunes = data.fortunes || {}; // 保存された値を使う
 
